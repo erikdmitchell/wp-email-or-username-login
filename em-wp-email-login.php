66ea3217950ac903e038501867206c610fe4e986
erikdmitchell <erikdmitchell@gmail.com>
2016-02-15 15:00:04 -0500

    init
    plugin works and is ready for distribution

---
+++ em-wp-email-login.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ Plugin Name: WP Email or Username Login
+ Plugin URI:
+ Description: Allows users to login by their username or email.
+ Version: 0.1.0
+ Author: Erik Mitchell
+ Author URI: http://erikmitchell.net
+ License: GPLv2 or later
+ Text Domain: ememaillogin
+*/
+
+/**
+ * mdw_allow_email_login filter to the authenticate filter hook, to fetch a username based on entered email
+ * @param  obj $user
+ * @param  string $username [description]
+ * @param  string $password [description]
+ * @return boolean
+ */
+function em_allow_email_login($user,$username,$password) {
+	if (is_email($username)) :
+		$user=get_user_by_email($username);
+		if ($user) :
+			$username=$user->user_login;
+		endif;
+	endif;
+
+	return wp_authenticate_username_password(null,$username,$password);
+}
+add_filter('authenticate','em_allow_email_login',20,3);
+
+
+/**
+ * em_add_email_to_login function to add email address to the username label
+ * @param string $translated_text   translated text
+ * @param string $text              original text
+ * @param string $domain            text domain
+ */
+function em_add_email_to_login($translated_text,$text,$domain) {
+	if ("Username"==$translated_text) :
+		$translated_text.= __(' Or Email','ememaillogin');
+	endif;
+
+	return $translated_text;
+}
+add_filter('gettext','em_add_email_to_login',20,3);
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== WP Email or Username Login ===
+Contributors: erikdmitchell
+Donate link: erikdmitchell@gmail.com
+Tags: login, email, username
+Requires at least: 4.0
+Tested up to: 4.4.2
+Stable tag: 4.4.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows users to login by their username or email.
+
+== Description ==
+
+Users can login with their email or username.
+
+== Installation ==
+
+1. Upload `em-wp-email-login` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+None yet. Just ask.
+
+== Screenshots ==
+
+Coming soon...
+
+== Hooks and Filters ==
+
+Coming soon...
+
+== Changelog ==
+
+= 0.1.0 =
+
+* Initial version.
+
+== Upgrade Notice ==
+
+None yet.
